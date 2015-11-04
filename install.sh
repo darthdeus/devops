20e785a2ba6777b5774665b89873b13586ed9d2b
Jakub Arnold <darthdeus@gmail.com>
2015-11-04 20:57:00 +0000

    Create ~/.vim/backup when installing

--- install.sh
+++ install.sh
@@ -10,5 +10,7 @@ if [ ! -d "$DEVOPS" ]; then
     git clone https://github.com/darthdeus/devops.git "$DEVOPS"
 fi
 
+mkdir -p "$HOME/.vim/backup"
+
 ln -nsf "$DEVOPS/vimrc" "$HOME/.vimrc"
 ln -nsf "$DEVOPS/zshrc" "$HOME/.zshrc"
