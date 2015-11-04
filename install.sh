20ccb9881b2b2f325937fcdb27427d77426af23a
Jakub Arnold <darthdeus@gmail.com>
2015-11-04 22:08:24 +0000

    Add missing install of vim-plug

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@ if [ ! -z "$HOST_COLOR" ]; then
     echo "$HOST_COLOR" > "$HOME/.host_color"
 fi
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p "$HOME/.vim/backup"
+
 if [ ! -d "$DEVOPS" ]; then
     git clone https://github.com/darthdeus/devops.git "$DEVOPS"
 fi
-
-mkdir -p "$HOME/.vim/backup"
 
 ln -nsf "$DEVOPS/vimrc" "$HOME/.vimrc"
 ln -nsf "$DEVOPS/zshrc" "$HOME/.zshrc"
